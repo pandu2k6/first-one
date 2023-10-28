f32007d095dc84fee5a7ecd61d13732b300f545b
pandu2k6 <149037949+pandu2k6@users.noreply.github.com>
2023-10-28 18:25:39 +0530

    Add files via upload

---
+++ even_or_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("it is even");
+    }
+    else{
+        printf("it is odd");
+
+    }
+}
---
+++ swaping.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter a number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    c=b;
+    d=a;
+    printf("a: %d\n",c);
+    printf("b: %d",d);
+
+  return 0;      
+}
